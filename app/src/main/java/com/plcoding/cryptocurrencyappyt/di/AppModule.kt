f36a6e394e2430163a301263e71d9f2947761c02
AndroidPhrophet <ketuimevbore@yahoo.com>
2021-09-16 20:46:39 -0700

    Composables completed for the view layer

---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/di/AppModule.kt
@@ -0,0 +1,38 @@
+package com.plcoding.cryptocurrencyappyt.di
+
+import androidx.compose.ui.unit.Constraints
+import com.plcoding.cryptocurrencyappyt.common.Constants
+import com.plcoding.cryptocurrencyappyt.data.remote.CoinPaprikaApi
+import com.plcoding.cryptocurrencyappyt.data.repository.CoinRepositoryImpl
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+    @Provides
+    @Singleton
+    fun providesPaprikaApi(): CoinPaprikaApi{
+        return  Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CoinPaprikaApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun providesCoinRepository(api: CoinPaprikaApi): CoinRepository{
+        return  CoinRepositoryImpl(api)
+    }
+
+
+}
---
+++ app/src/main/java/com/plcoding/cryptocurrencyappyt/domain/usecases/getcoin/GetCoinUseCase.kt
@@ -0,0 +1,35 @@
+package com.plcoding.cryptocurrencyappyt.domain.usecases.getcoin
+
+import com.plcoding.cryptocurrencyappyt.common.Resource
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoin
+import com.plcoding.cryptocurrencyappyt.data.remote.dto.toCoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.model.Coin
+import com.plcoding.cryptocurrencyappyt.domain.model.CoinDetail
+import com.plcoding.cryptocurrencyappyt.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(private val  repository: CoinRepository){
+
+    operator fun invoke (coinId: String): Flow<Resource<CoinDetail>> = flow {
+
+        try {
+            emit(Resource.Loading<CoinDetail>())
+
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+
+            emit(Resource.Success(coin))
+
+        }catch (e: HttpException) {
+            emit(Resource.Error<CoinDetail>(e.localizedMessage ?: "An unexpected error occurred"))
+
+        }catch (e: IOException){
+            emit(Resource.Error<CoinDetail>("Couldn't reach server. Check your internet connection and try again"))
+
+        }
+
+    }
+}
